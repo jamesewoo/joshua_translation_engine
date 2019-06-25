f45ff58eff7bcf0344edd5dd6040cb48192a1654
James Woo <james.e.woo@gmail.com>
2019-06-25 22:39:02 +0000

    Update compose dependencies

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ if [ "$1" = 'pytest' ]; then
 elif [ "$1" = 'es-test' ]; then
 
     pipenv run gunicorn -b "0.0.0.0:8000" --forwarded-allow-ips="*" "app:create_app()" &
-    sleep 2
+    sleep 3
     curl localhost:8000/translate/english -iv -H "Content-Type: application/json" -X POST -d '{"inputLanguage": "Spanish", "inputText": "vuelo"}'
 
 elif [ "$1" = 'deploy' ]; then
